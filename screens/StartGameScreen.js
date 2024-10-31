a0485d0adc7591ca66b73a5e0314c02364e22463
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-31 23:56:42 +0000

    Nov 1, 2024, 5:26 AM

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput style={styles.numberInput} />
             <PrimaryButton>React</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -23,5 +23,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#72063c',
         borderRadius: 8,
         elevation: 4,
-    }
+    },
+
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32, 
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 });
