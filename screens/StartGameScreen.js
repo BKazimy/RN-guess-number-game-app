aa49b2d3cb716411009f09d0cb94554cfaa31bbe
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-01 21:09:06 +0000

    Nov 2, 2024, 2:39 AM

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import { TextInput, View, StyleSheet, Alert } from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen({ onConfermNumbebr }) {
+function StartGameScreen({ onConfermNumber }) {
     const [enteredNumber, setEnteredNumber] = useState('')
 
     function numberInputHandler(enteredText) {
@@ -26,7 +26,7 @@ function StartGameScreen({ onConfermNumbebr }) {
         }
         else {
             console.log("Valid number!")
-            onConfermNumbebr(chosenNumber);
+            // onConfermNumber(chosenNumber);
         }
     }
 
