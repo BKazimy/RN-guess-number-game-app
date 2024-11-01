3f83afb3381781dc887a09eb57e3db329513eec1
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-01 19:29:12 +0000

    Nov 2, 2024, 12:59 AM

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
+     // styling here will apply throughout the whole app
     <View style={styles.rootScreen}>
       <StartGameScreen />
     </View>
@@ -12,6 +13,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   rootScreen: {
+    flex: 1,
     backgroundColor: '#ddb52f',
   }
 });
