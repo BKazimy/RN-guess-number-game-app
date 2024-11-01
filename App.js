1f8a0a168cb06485536b22598053841136303ab3
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-01 19:59:43 +0000

    Nov 2, 2024, 1:29 AM

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StyleSheet } from 'react-native';
-import { LinrearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
      // styling here will apply throughout the whole app
-    <LinrearGradient 
+    <LinearGradient 
       style={styles.rootScreen}
       colors={[]}
     >
       <StartGameScreen />
-    </LinrearGradient>
+    </LinearGradient>
   );
 }
 
