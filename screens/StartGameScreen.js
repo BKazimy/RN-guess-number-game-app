7402207b5bb75a1b96a221890014a0bf51f173ed
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-01 18:41:59 +0000

    Nov 2, 2024, 12:11 AM

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} />
+            <TextInput 
+                style={styles.numberInput} 
+                maxLength={2} 
+                keyboardType='number-pad'
+                autoCapitalize='none'
+                autoCorrect={false} // prevents form auto correnion useful for email input
+            />
             <PrimaryButton>React</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
